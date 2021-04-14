e73a4ea38e7017635ed3636affb44fab17f8f57e
Miguel Angel Arcos <moneite@gmail.com>
2021-04-14 13:51:47 +0200

    inicio cine

---
+++ proyectoweb/js/cine.js
@@ -0,0 +1,18 @@
+$('.navTrigger').click(function() {
+    $(this).toggleClass('active');
+    console.log("Clicked menu ");
+    $("#mainListDiv ").toggleClass("show_list ");
+    $("#mainListDiv ").fadeIn();
+
+});
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".nav ");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+
+AOS.init({
+    duration: 1200,
+});
