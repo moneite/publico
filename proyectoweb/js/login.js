5939a2e3f3e84edff377480dd3add63056745c80
Miguel Angel Arcos <moneite@gmail.com>
2021-05-11 14:42:38 +0200

    Login-logout

--- proyectoweb/js/login.js
+++ proyectoweb/js/login.js
@@ -1,3 +1,30 @@
+window.onload = function comprobarLogin() {
+
+    if (sessionStorage.getItem("user").length > 0) {
+        $('#loginOculto').css('display', 'none');
+        $('#logout').css('display', 'inline-block');
+        $('#conectado').css('display', 'inline-block');
+    }
+}
+document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
+
+
+function cerrarSesion() {
+    if (typeof(Storage) !== "undefined") {
+        if (confirm("¿Desea Cerrar Sesión?")) {
+            sessionStorage.removeItem("user");
+            location.reload();
+        } else {
+
+        }
+
+    } else {
+        alert("Este navegador no soporta web storage...");
+    }
+
+    return false;
+}
+
 function leerXML() {
 
 
@@ -28,14 +55,24 @@ function miFuncion(xml) {
             if (x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue == document.getElementById("pass").value) {
                 loginAceptado = true;
                 nombre = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+                pass = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
+                if (typeof(Storage) !== 'undefined') {
+                    // Código cuando Storage es compatible
+                    sessionStorage.setItem('user', nombre);
+                    sessionStorage.setItem('clave', pass);
+                } else {
+                    alert("El navegador no es compatible con SessionStorage.")
+                        // Código cuando Storage NO es compatible
+                }
                 break;
+
             }
         }
     }
 
     if (loginAceptado == true) {
-        window.alert("Bienvenido " + nombre);
-        window.location.href = "index.html";
+        window.alert("Bienvenid@ " + nombre);
+        window.history.go(-1);;
     } else {
         window.alert("Usuario o contraseña incorrectos");
     }
